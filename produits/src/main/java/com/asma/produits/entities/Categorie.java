bfe4688f293f707be721dc8909a767296f5f08be
asmasellami <114883779+asmasellami@users.noreply.github.com>
2023-10-10 21:40:02 +0200

    Update Categorie.java

--- produits/src/main/java/com/asma/produits/entities/Categorie.java
+++ produits/src/main/java/com/asma/produits/entities/Categorie.java
@@ -31,47 +31,5 @@ public class Categorie {
 	@OneToMany(mappedBy = "categorie")
 	private List<Produit> produits;
 
-	public Categorie() {
-		super();
-	}
-
-	public Categorie(String nomCat, String descriptionCat, List<Produit> produits) {
-		super();
-		this.nomCat = nomCat;
-		this.descriptionCat = descriptionCat;
-		this.produits = produits;
-	}
-
-	public Long getIdCat() {
-		return idCat;
-	}
-
-	public void setIdCat(Long idCat) {
-		this.idCat = idCat;
-	}
-
-	public String getNomCat() {
-		return nomCat;
-	}
-
-	public void setNomCat(String nomCat) {
-		this.nomCat = nomCat;
-	}
-
-	public String getDescriptionCat() {
-		return descriptionCat;
-	}
-
-	public void setDescriptionCat(String descriptionCat) {
-		this.descriptionCat = descriptionCat;
-	}
-
-	public List<Produit> getProduits() {
-		return produits;
-	}
-
-	public void setProduits(List<Produit> produits) {
-		this.produits = produits;
-	}
 
 }
